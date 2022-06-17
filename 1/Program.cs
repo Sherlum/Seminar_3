79297c8f8b8f8562915488bc54fe813abbeb3d5d
Stanislav <hansolo99@yandex.ru>
2022-06-17 19:50:14 +0300

    1 and 3 complited

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите пятизначное число: ");
+string paramString = Console.ReadLine();
+int paramInt = int.Parse(paramString);
+int n = paramInt;
+int revers = 0;
+int dig = 0;
+int num = paramInt;
+if (paramString.Length != 5)
+Console.WriteLine("Введено неверное число!");
+
+else
+ while (num > 0)
+ {
+      dig = num % 10;
+      revers = revers * 10 + dig;
+      num = num / 10;
+ }
+if (n == revers)
+Console.WriteLine($"{paramInt} палиндром");
+else
+Console.WriteLine($"{paramInt} не палиндром");
--- 3/Program.cs
+++ 3/Program.cs
@@ -2,4 +2,14 @@
 // 3 -> 1, 8, 27. 
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число N");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+            Console.WriteLine("{0}^2 = {1}", i, i*i*i);
+        Console.ReadKey(true);
+    }
+}
